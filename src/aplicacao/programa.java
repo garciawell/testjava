583cc3c939b771d01e9584cd04ab29b283910e9e
Wellington Garcia <garciawell@gmail.com>
2020-02-01 15:45:04 -0300

    Find user db

--- src/aplicacao/programa.java
+++ src/aplicacao/programa.java
@@ -18,12 +18,12 @@ public class programa {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
         EntityManager em = emf.createEntityManager();
 
-        em.getTransaction().begin();
-        em.persist(p1);
-        em.persist(p2);
-        em.persist(p3);
-        em.getTransaction().commit();
+        Pessoa p = em.find(Pessoa.class, 2);
+
+        System.out.println(p);
         System.out.println("Pronto!");
+        em.close();
+        emf.close();
     }
 
 }
